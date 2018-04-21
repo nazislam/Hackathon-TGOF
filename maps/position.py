c5a1ce6fe727e63c454559e8b25e8b39b826b6ff
votrungquan1999 <votrungquan99@gmail.com>
2018-04-21 16:22:22 -0400

    maps hard code

--- maps/position.py
+++ maps/position.py
@@ -2,3 +2,15 @@ class Position:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
+    def setx(self, x):
+        self.x = x
+
+    def sety(self, y):
+        self.y = y
--- maps/terrain.py
+++ maps/terrain.py
@@ -7,25 +7,38 @@ class Terrain:
     def get_type(self):
         return self.type
 
+    def __str__(self):
+        return self.type
+
 class Grass(Terrain):
     def __init__(self):
-        Terrain.__init__()
+        Terrain.__init__(self)
         self.type = "Grass"
+
+    def get_type(self):
+        return self.type
 
 class Swamp(Terrain):
     def __init__(self):
-        Terrain.__init__()
+        Terrain.__init__(self)
         self.type = "Swamp"
         self.speed_rate = 0.5
 
+
 class Mountain(Terrain):
     def __init__(self):
-        Terrain.__init__()
-        self.type = "Mountain"
+        Terrain.__init__(self)
+        self.type = "Mount"
         self.stepable = False
+
 
 class River(Terrain):
     def __init__(self):
-        Terrain.__init__()
+        Terrain.__init__(self)
         self.type = "River"
-        self.stepable = "True"
+        self.stepable = True
+
+
+if __name__ == '__main__':
+    terrain = Grass()
+    print(terrain.get_type())
